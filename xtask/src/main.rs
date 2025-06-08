fbd03829752b89e6a0161fd78ed0ec89e3da6b9d
junbo.dai <bobodai515@163.com>
2025-06-08 17:44:13 +0800

    feat: add command in xtask to echo qemu done

---
+++ xtask/src/main.rs
@@ -0,0 +1,26 @@
+use anyhow::Result;
+use clap::{Parser, Subcommand};
+use std::process::Command;
+
+#[derive(Parser)]
+#[command(name = "xtask")]
+#[command(about="Build tools for the project", long_about=None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    #[clap(about = "Run the qemu")]
+    Qemu,
+}
+
+fn main() -> Result<()> {
+    match Cli::parse().command {
+        Commands::Qemu => {
+            let status = Command::new("echo").arg("qemu done").status();
+        }
+    }
+    Ok(())
+}
